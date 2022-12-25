7c872fbd470e8a1179afa20806442f0890973f1c
Rubel Hossain99 <rubel820746@gmail.com>
2022-12-25 23:58:41 +0600

    Testimonial and Client Section is Done

---
+++ assets/js/clientslider.js
@@ -0,0 +1,17 @@
+$('.test-crosel').owlCarousel({
+    loop: true,
+    margin: 0,
+    nav: true,
+    dots: false,
+    responsive: {
+        0: {
+            items: 1
+        },
+        600: {
+            items: 2
+        },
+        1000: {
+            items: 2
+        }
+    }
+})
